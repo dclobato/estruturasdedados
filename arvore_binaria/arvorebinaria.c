69ae4548eac1748452971b6244c0e1ab5e7e9672
Daniel Corea Lobato <daniel@lobato.org>
2015-10-29 15:55:36 -0200

    arvore binaria de busca

--- arvore_binaria/arvorebinaria.c
+++ arvore_binaria/arvorebinaria.c
@@ -4,83 +4,135 @@
 
 void inicializa_arvore (ARVORE *t, int *status)
 {
-   *t = NULL;
-   *status = 1;
-   return;
+    *t = NULL;
+    *status = 1;
+    return;
 }
 
 ARVORE aloca_no_arvore (int v, int *status)
 {
-   ARVORE t;
-   
-   t = malloc (sizeof (NO_ARVORE));
-   if (t == NULL)
-   {
-      *status = 0;
-   }
-   else
-   {
-      t->dado = v;
-      t->esq = NULL;
-      t->dir = NULL;
-      *status = 1;
-   }
-   return (t);
+    ARVORE t;
+
+    t = malloc (sizeof (NO_ARVORE));
+    if (t == NULL)
+    {
+        *status = 0;
+    }
+    else
+    {
+        t->dado = v;
+        t->esq = NULL;
+        t->dir = NULL;
+        *status = 1;
+    }
+    return (t);
+}
+
+void insere_abb (ARVORE *t, ARVORE *f, int *status)
+{
+    ARVORE p, antes;
+    // Se arvore esta vazia, manda bala ai mesmo
+    if (*t == NULL)
+    {
+        printf("Inserindo raiz da ABB\n");
+        definir_arvore (t, RAIZ, f, status);
+        return;
+    }
+
+    // Caso contrario, vamos procurar onde colocar
+    p = *t;
+    antes = NULL;
+    while (p != NULL)
+    {
+        if (p->dado == (*f)->dado)
+        {
+            *status = 0;
+            return;
+        }
+        antes = p;
+        if ((*f)->dado < p->dado)
+            p = p->esq;
+        else
+            p = p->dir;
+    }
+    if ((*f)->dado < antes->dado)
+        definir_arvore (&antes, ESQ, f, status);
+    else
+        definir_arvore (&antes, DIR, f, status);
 }
 
 void definir_arvore (ARVORE *t, TipoNo tipono, ARVORE *f, int *status)
 {
-   *status = 0;
-   if (((tipono == ESQ) || (tipono == DIR)) && (*t == NULL))
-      return;
-   if ((tipono == RAIZ) && (*t == NULL))
-   {
-      *t = *f;
-      *status = 1;
-   }
-   if ((tipono == ESQ) && ((*t)->esq == NULL))
-   {
-      (*t)->esq = *f;
-      *status = 1;
-   }
-   if ((tipono == DIR) && ((*t)->dir == NULL))
-   {
-      (*t)->dir = *f;
-      *status = 1;
-   }
-   return;
+    *status = 0;
+    if (((tipono == ESQ) || (tipono == DIR)) && (*t == NULL))
+        return;
+    if ((tipono == RAIZ) && (*t == NULL))
+    {
+        *t = *f;
+        *status = 1;
+    }
+    if ((tipono == ESQ) && ((*t)->esq == NULL))
+    {
+        (*t)->esq = *f;
+        *status = 1;
+    }
+    if ((tipono == DIR) && ((*t)->dir == NULL))
+    {
+        (*t)->dir = *f;
+        *status = 1;
+    }
+    return;
 }
-   
+
 void percurso_pre (ARVORE t)
 {
-   if (t != NULL)
-   {
-       printf("%d ", t->dado);
-       percurso_pre (t->esq);
-       percurso_pre (t->dir);
-   }
-   return;
+    if (t != NULL)
+    {
+        printf("%d ", t->dado);
+        percurso_pre (t->esq);
+        percurso_pre (t->dir);
+    }
+    return;
 }
 
 void percurso_in (ARVORE t)
 {
-   if (t != NULL)
-   {
-       percurso_in (t->esq);
-       printf("%d ", t->dado);
-       percurso_in (t->dir);
-   }
-   return;
+    if (t != NULL)
+    {
+        percurso_in (t->esq);
+        printf("%d ", t->dado);
+        percurso_in (t->dir);
+    }
+    return;
 }
 
 void percurso_pos (ARVORE t)
 {
-   if (t != NULL)
-   {
-       percurso_pos (t->esq);
-       percurso_pos (t->dir);
-       printf("%d ", t->dado);
-   }
-   return;
+    if (t != NULL)
+    {
+        percurso_pos (t->esq);
+        percurso_pos (t->dir);
+        printf("%d ", t->dado);
+    }
+    return;
 }
 
+void _imprime_arvore( ARVORE treePtr, int *row)
+{
+    if (treePtr != NULL) {
+        (*row)++;
+        _imprime_arvore(treePtr->dir,row);
+        int i;
+        for(i=0; i<(3*(*row)); i++)
+            printf(" ");
+
+        printf("%d\n", treePtr->dado);
+        _imprime_arvore(treePtr->esq,row);
+        (*row)--;
+    }
+}
+
+void imprime_arvore(ARVORE treePtr) {
+    int row = 0;
+    _imprime_arvore(treePtr,&row);
+}
--- arvore_binaria/arvorebinaria.h
+++ arvore_binaria/arvorebinaria.h
@@ -3,9 +3,9 @@
 typedef enum {RAIZ, ESQ, DIR} TipoNo;
 
 typedef struct __noar {
-   int dado;
-   struct __noar *esq;
-   struct __noar *dir;
+    int dado;
+    struct __noar *esq;
+    struct __noar *dir;
 } NO_ARVORE;
 
 typedef NO_ARVORE* ARVORE;
@@ -13,7 +13,10 @@ typedef NO_ARVORE* ARVORE;
 void inicializa_arvore (ARVORE *, int *);
 ARVORE aloca_no_arvore (int, int*);
 void definir_arvore (ARVORE *, TipoNo, ARVORE *, int *);
+void insere_abb (ARVORE *, ARVORE *, int *);
 void percurso_pre (ARVORE);
 void percurso_in (ARVORE);
 void percurso_pos (ARVORE);
+void imprime_arvore(ARVORE);
+
 #endif
